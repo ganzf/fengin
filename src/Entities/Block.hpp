ca74fa45d91cf2138e4c6bcf759482ba95cc60fd
Arroganz <felix.ganz@epitech.eu>
2018-08-04 18:45:10 +0200

    IsoCam now behaving exactly like NormalCam

--- src/Entities/Block.hpp
+++ src/Entities/Block.hpp
@@ -12,7 +12,13 @@ namespace fengin::entities {
     class Block: public futils::IEntity {
     public:
         Block(futils::Vec3d<float> position) {
-            attach<fengin::components::Transform>();
+            auto &tr = attach<fengin::components::Transform>();
+            tr.size.x = 1;
+            tr.size.y = 1;
+            tr.size.z = 1;
+            tr.position.x = -0.5f;
+            tr.position.y = -0.5f;
+            tr.position.z = 0;
         }
     };
 }
